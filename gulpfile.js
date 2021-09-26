06b3870b84d4217d7a1e544c21d742c85e121cce
andfres <andfres@gmail.com>
2021-09-26 22:31:01 +0200

    configuracion gulp

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const nunjucks = require('gulp-nunjucks');
+
+
+gulp.task('sass', function(){
+
+    console.log("ejecutando sass")
+
+    
+    return gulp.src('src/**/*.scss' , {base: './src'})
+        .pipe(sass().on('error',sass.logError))
+        .pipe(gulp.dest('dist'));
+
+});
+
+gulp.task('nunjucks', function(){
+
+    console.log("ejecutando nunjucks")
+
+    
+    return gulp.src(['src/**/*.njk' ,'!src/build/**/*']  , {base: './src'})
+        .pipe(nunjucks.compile())
+        .pipe(gulp.dest('dist/'));
+
+});
+
+
+gulp.task('all:watch', function(){
+    gulp.watch('src/**/*.scss',  gulp.series('sass')),
+    gulp.watch([  'src/**/*.njk' ] , gulp.series('nunjucks'));
+    return;
+
+});
+
+
+
+
+
+/*
+gulp.task('dihola', function(){
+
+    //console.log("Hola");
+    //return Promise.resolve();
+
+    return new Promise((resolve, reject) => {
+
+        console.log("Hola");
+
+        setTimeout(()=>{
+            console.log("Adios");
+            resolve();
+        },1000);
+
+    });
+
+});
+
+*/
+
+
+
